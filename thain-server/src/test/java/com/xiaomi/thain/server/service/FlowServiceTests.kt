f876f7956816a0bd893e52d1acd5d349dfafc577
liangyongrui <leungyongrui@gmail.com>
2019-12-09 20:40:37 +0800

    update unit test

--- thain-server/src/test/java/com/xiaomi/thain/server/service/FlowServiceTests.kt
+++ thain-server/src/test/java/com/xiaomi/thain/server/service/FlowServiceTests.kt
@@ -3,66 +3,63 @@
  * This source code is licensed under the Apache License Version 2.0, which
  * can be found in the LICENSE file in the root directory of this source tree.
  */
+package com.xiaomi.thain.server.service
 
-package com.xiaomi.thain.server.service;
+import com.alibaba.fastjson.JSON
+import com.xiaomi.thain.common.constant.FlowSchedulingStatus
+import com.xiaomi.thain.common.exception.ThainException
+import com.xiaomi.thain.common.exception.ThainRepeatExecutionException
+import com.xiaomi.thain.common.model.FlowModel
+import com.xiaomi.thain.common.model.JobModel
+import com.xiaomi.thain.common.model.rq.AddFlowRq
+import com.xiaomi.thain.common.model.rq.AddJobRq
+import com.xiaomi.thain.server.Application
+import com.xiaomi.thain.server.service.impl.FlowServiceImpl
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.quartz.SchedulerException
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import java.io.IOException
+import java.text.ParseException
+import java.util.concurrent.TimeUnit
 
-import com.alibaba.fastjson.JSON;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.xiaomi.thain.common.constant.FlowSchedulingStatus;
-import com.xiaomi.thain.common.exception.ThainException;
-import com.xiaomi.thain.common.exception.ThainRepeatExecutionException;
-import com.xiaomi.thain.common.model.JobModel;
-import com.xiaomi.thain.common.model.rq.AddJobRq;
-import com.xiaomi.thain.server.Application;
-import com.xiaomi.thain.server.service.impl.FlowServiceImpl;
-import lombok.val;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.quartz.SchedulerException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.concurrent.TimeUnit;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
-public class FlowServiceTests {
-
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [Application::class])
+class FlowServiceTests {
     @Autowired
-    private FlowServiceImpl flowService;
+    private val flowService: FlowServiceImpl? = null
 
     @Test
-    public void test() throws ParseException, ThainException, SchedulerException, InterruptedException, ThainRepeatExecutionException, IOException {
-//        val addFlowRq = JSON.parseObject(JSON.toJSONString(AddFlowRq.builder()
-//                .name("test")
-//                .cron("* * * * * ?")
-//                .createUser("admin")
-//                .build()), com.xiaomi.thain.common.model.rq.kt.AddFlowRq.class);
-//        val jobs = ImmutableList.of(
-//                JSON.parseObject(
-//                        JSON.toJSONString(JobModel.builder()
-//                                .name("test")
-//                                .component("std::http")
-//                                .properties(ImmutableMap.of(
-//                                        "method", "GET",
-//                                        "contentType", "application/json",
-//                                        "url", "https://github.com"
-//                                )).build()), AddJobRq.class));
-//        val flowId = flowService.add(addFlowRq, jobs, "thain");
-//        TimeUnit.SECONDS.sleep(10);
-//        val flow = flowService.getFlow(flowId);
-//        Assert.assertEquals(flow.schedulingStatus, FlowSchedulingStatus.SCHEDULING.code);
-////        val flowId2 = flowService.add(addFlowRq.toBuilder().id(flowId).build(), jobs, "thain");
-////        Assert.assertEquals(flowId, flowId2);
-//        Assert.assertEquals(flow.schedulingStatus, FlowSchedulingStatus.SCHEDULING.code);
-//        flowService.pause(flowId);
-//        flowService.scheduling(flowId);
-//        flowService.delete(flowId);
+    @Throws(ParseException::class, ThainException::class, SchedulerException::class, InterruptedException::class, ThainRepeatExecutionException::class, IOException::class)
+    fun test() {
+        val addFlowRq = JSON.parseObject(JSON.toJSONString(FlowModel.builder()
+                .name("test")
+                .cron("* * * * * ?")
+                .createUser("admin")
+                .build()), AddFlowRq::class.java)
+        val jobs = listOf(
+                JSON.parseObject(
+                        JSON.toJSONString(JobModel.builder()
+                                .name("test")
+                                .component("std::http")
+                                .properties(mapOf(
+                                        "method" to "GET",
+                                        "contentType" to "application/json",
+                                        "url" to "https://www.mi.com"
+                                )).build()), AddJobRq::class.java))
+        val flowId = flowService!!.add(addFlowRq, jobs, "thain")
+        TimeUnit.SECONDS.sleep(10)
+        val flow = flowService.getFlow(flowId)
+        Assert.assertEquals(flow.schedulingStatus.toLong(), FlowSchedulingStatus.SCHEDULING.code.toLong())
+        val flowId2 = flowService.add(addFlowRq.copy(id = flowId), jobs, "thain");
+        Assert.assertEquals(flowId, flowId2);
+        val flow2 = flowService.getFlow(flowId)
+        Assert.assertEquals(flow2.schedulingStatus.toLong(), FlowSchedulingStatus.SCHEDULING.code.toLong())
+        flowService.pause(flowId)
+        flowService.scheduling(flowId)
+        flowService.delete(flowId)
     }
-
 }

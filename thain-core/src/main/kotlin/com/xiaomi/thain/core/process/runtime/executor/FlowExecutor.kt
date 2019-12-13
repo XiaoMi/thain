08110e4187cb0c10b1a377db947650f2970ea2bd
liangyongrui <leungyongrui@gmail.com>
2019-12-13 12:31:50 +0800

    end flow

--- thain-core/src/main/kotlin/com/xiaomi/thain/core/process/runtime/executor/FlowExecutor.kt
+++ thain-core/src/main/kotlin/com/xiaomi/thain/core/process/runtime/executor/FlowExecutor.kt
@@ -73,7 +73,6 @@ class FlowExecutor private constructor(flowExecutionDr: FlowExecutionDr,
         } finally {
             try {
                 flowExecutionService.endFlowExecution()
-
             } finally {
                 flowExecutionJobThreadPool.shutdown()
                 FlowExecutionStorage.drop(flowExecutionId)

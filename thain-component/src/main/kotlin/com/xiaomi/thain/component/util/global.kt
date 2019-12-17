165d153f866e7804cc3af29be24ed559d8810b0d
liangyongrui <leungyongrui@gmail.com>
2019-12-17 14:34:47 +0800

    fix ClassCastException

--- thain-component/src/main/kotlin/com/xiaomi/thain/component/util/global.kt
+++ thain-component/src/main/kotlin/com/xiaomi/thain/component/util/global.kt
@@ -3,12 +3,12 @@ package com.xiaomi.thain.component.util
 import com.xiaomi.thain.common.utils.ifNull
 
 fun formatHttpReferenceData(referenceData: String?,
-                            getStorageValueOrDefault: (String, String, String) -> String): Map<String, String> {
+                            getStorageValueOrDefault: (String, String, Any) -> Any): Map<String, String> {
     return referenceData.ifNull { "" }
             .split(",".toRegex())
             .map { it.trim() }
             .map { it.split("[:.]".toRegex()) }
             .filter { it.size == 3 }
-            .map { it[0] to getStorageValueOrDefault(it[1], it[2], "") }
+            .map { it[0] to getStorageValueOrDefault(it[1], it[2], "").toString() }
             .toMap()
 }
